dad3364a23070ead04314181d89f9a6bf21a605f
yashjoshi-dotcom <yashjsohimail@gmail.com>
2022-11-05 16:44:56 +0530

    set routes and removed redundant files

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import LandingPage from "./pages/LandingPage";
+import Listing from "./pages/Listing";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar/> 
+    <Routes>
+    <Route path="/" element={<LandingPage />} />      
+    <Route path="/listing" element={<Listing/>} />        
+    <Route path="/profile" element={<Profile/> } />        
+    <Route path="/singin" element={<Signin/> } />        
+   </Routes>                                                       
+    </>
   );
 }
 
---
+++ client/src/pages/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div>
+        <h1>
+            This is landing Page..  
+        </h1>
+    </div>
+  )
+}
+
+export default LandingPage
