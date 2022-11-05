922bc983e15b7cdf088493c3edb670d1c87f758e
Yuvi <81169447+YUVRAJg37@users.noreply.github.com>
2022-11-05 16:03:48 +0530

    init

--- dev/Source/EduCapture/EduCapture.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "EduCapture.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EduCapture, "EduCapture" );
